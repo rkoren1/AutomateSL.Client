0c880f91aa5a9699ceaacd4cdd7af3110211acdc
rkoren1 <rok.koren@netica.si>
2022-12-01 18:23:11 +0100

    working logout

--- src/app/core/interceptors/token-interceptor.ts
+++ src/app/core/interceptors/token-interceptor.ts
@@ -24,7 +24,7 @@ export class TokenInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const handler = () => {
-      if (request.url.includes('/auth/logout')) {
+      if (request.url.includes('/api/logout')) {
         this.router.navigateByUrl('/auth/login');
       }
 
