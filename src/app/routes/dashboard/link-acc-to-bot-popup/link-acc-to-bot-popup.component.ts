769ccdbb6ba9941eca43b13276d2560e3d8b2808
rkoren1 <rok.koren@netica.si>
2022-12-06 22:05:57 +0100

    fixed type in formgroup

--- src/app/routes/dashboard/link-acc-to-bot-popup/link-acc-to-bot-popup.component.ts
+++ src/app/routes/dashboard/link-acc-to-bot-popup/link-acc-to-bot-popup.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LinkAccData } from '@shared/Models/bot.model';
+import { LinkAccForm } from '@shared/Models/forms.model';
 import { LinkAccToBotPopupService } from './link-acc-to-bot-popup.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { LinkAccToBotPopupService } from './link-acc-to-bot-popup.service';
   styleUrls: ['./link-acc-to-bot-popup.component.scss'],
 })
 export class LinkAccToBotPopupComponent implements OnInit {
-  linkAccForm: FormGroup; //<LinkAccForm> why it doesnt work???
+  linkAccForm: FormGroup<LinkAccForm>;
   constructor(
     private dialogRef: MatDialogRef<LinkAccToBotPopupComponent>,
     private linkAccToBotPopupService: LinkAccToBotPopupService,
@@ -18,13 +19,25 @@ export class LinkAccToBotPopupComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.linkAccForm = new FormGroup({
-      loginFirstName: new FormControl(this.data.botConfiguration.loginFirstName),
-      loginLastName: new FormControl(this.data.botConfiguration.loginLastName),
-      loginPassword: new FormControl(this.data.botConfiguration.loginPassword),
-      loginStartLocation: new FormControl(this.data.botConfiguration.loginStartLocation),
+    this.initForm();
+  }
+  private initForm() {
+    this.linkAccForm = new FormGroup<LinkAccForm>({
+      loginFirstName: new FormControl(this.data.botConfiguration.loginFirstName, {
+        nonNullable: true,
+      }),
+      loginLastName: new FormControl(this.data.botConfiguration.loginLastName, {
+        nonNullable: true,
+      }),
+      loginPassword: new FormControl(this.data.botConfiguration.loginPassword, {
+        nonNullable: true,
+      }),
+      loginStartLocation: new FormControl(this.data.botConfiguration.loginStartLocation, {
+        nonNullable: true,
+      }),
     });
   }
+
   validateForm() {
     this.linkAccForm.markAllAsTouched();
     if (this.linkAccForm.valid) {
