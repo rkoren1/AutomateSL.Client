5cb44461955c695cf659642e3a4ebb0adc3f426b
rkoren1 <rok.koren@netica.si>
2022-12-07 09:55:01 +0100

    changed environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: '',
   useHash: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://zen-bell.209-239-116-141.plesk.page/api',
 };
