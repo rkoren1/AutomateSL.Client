2daa7bd05c6cfcc0b618a791a84f311a596c474a
rkoren1 <rok.koren@netica.si>
2022-12-11 10:21:22 +0100

    change environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: '',
   useHash: false,
-  apiUrl: 'https://zen-bell.209-239-116-141.plesk.page/api',
+  apiUrl: 'https://lifebots.app/api',
 };
