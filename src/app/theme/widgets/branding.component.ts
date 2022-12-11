4d7b3355109b27f483c1ee195f9989dec7612c99
rkoren1 <rok.koren@netica.si>
2022-12-11 11:27:43 +0100

    changed logo name

--- src/app/theme/widgets/branding.component.ts
+++ src/app/theme/widgets/branding.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-branding',
   template: `
     <a class="matero-branding" href="/">
-      <img src="./assets/images/logo.png" class="matero-branding-logo-expanded" alt="logo" />
+      <img
+        src="./assets/images/logo-dark-theme.png"
+        class="matero-branding-logo-expanded"
+        alt="logo"
+      />
       <span class="matero-branding-name">LifeBots</span>
     </a>
   `,
