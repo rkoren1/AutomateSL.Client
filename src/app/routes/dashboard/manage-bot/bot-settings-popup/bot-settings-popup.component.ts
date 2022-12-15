db4d8a36af681994e9fd718229757e2d3738521b
Rok Koren <rok.koren@netica.si>
2022-12-15 10:37:47 +0100

    work on bot settings popup

--- src/app/routes/dashboard/manage-bot/bot-settings-popup/bot-settings-popup.component.ts
+++ src/app/routes/dashboard/manage-bot/bot-settings-popup/bot-settings-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bot-settings-popup',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bot-settings-popup.component.scss'],
 })
 export class BotSettingsPopupComponent implements OnInit {
+  botSettingsForm: FormGroup;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initForm();
+  }
+  validateForm() {}
+  private initForm() {
+    this.botSettingsForm = new FormGroup({
+      acceptinventory: new FormControl(false, {
+        nonNullable: true,
+      }),
+      acceptGroupInvitations: new FormControl(false, {
+        nonNullable: true,
+      }),
+      acceptAnimations: new FormControl(false, {
+        nonNullable: true,
+      }),
+    });
+  }
 }
