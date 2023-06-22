2ffc1b0fa44bd2f267de4cecb2672dbeed074546
Rok Koren <rok.koren@netica.si>
2023-06-22 11:09:10 +0200

    added support in footer

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -4,27 +4,29 @@ import { SharedModule } from '@shared/shared.module';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
+import { SidebarNoticeComponent } from './sidebar-notice/sidebar-notice.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
-import { NavAccordionDirective } from './sidemenu/nav-accordion.directive';
 import { NavAccordionItemDirective } from './sidemenu/nav-accordion-item.directive';
 import { NavAccordionToggleDirective } from './sidemenu/nav-accordion-toggle.directive';
-import { SidebarNoticeComponent } from './sidebar-notice/sidebar-notice.component';
+import { NavAccordionDirective } from './sidemenu/nav-accordion.directive';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
-import { TopmenuComponent } from './topmenu/topmenu.component';
 import { TopmenuPanelComponent } from './topmenu/topmenu-panel.component';
+import { TopmenuComponent } from './topmenu/topmenu.component';
 
 import { HeaderComponent } from './header/header.component';
 
+import { FooterComponent } from './footer/footer.component';
+import { UserPanelComponent } from './sidebar/user-panel/user-panel.component';
 import { BrandingComponent } from './widgets/branding.component';
 import { NotificationComponent } from './widgets/notification.component';
 import { TranslateComponent } from './widgets/translate.component';
-import { UserPanelComponent } from './sidebar/user-panel/user-panel.component';
 import { UserComponent } from './widgets/user/user.component';
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
+    FooterComponent,
     AuthLayoutComponent,
     SidebarComponent,
     UserPanelComponent,
